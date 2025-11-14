94db57a4e117ebfc7b78c574d99298dda7da8931
M Riduan <mriduanpro@gmail.com>
2025-11-14 08:31:59 +0700

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,2 +1,5 @@
-// Adsterra Popunder
+// SocialBar
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//hustlereadcoffeehouse.com/c6/7f/17/c67f17d6e15abcc9f97341db0404b073.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
+
+// Popunder
+(function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//hustlereadcoffeehouse.com/76/0a/c5/760ac5880d7732431eadd507e799cdd8.js"}],"tag":"script","inner":""}],{"target":"head"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
