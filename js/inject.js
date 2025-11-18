fe0305a3a18715df2717e831125155828ec8efdd
M Riduan <mriduanpro@gmail.com>
2025-11-18 21:44:59 +0700

    Update inject.js

--- js/inject.js
+++ js/inject.js
@@ -1,16 +1,16 @@
-// SocialBar
+/* SocialBar */
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//hustlereadcoffeehouse.com/c6/7f/17/c67f17d6e15abcc9f97341db0404b073.js"}],"tag":"script","inner":""}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-// Popunder
+/* Popunder */
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//hustlereadcoffeehouse.com/76/0a/c5/760ac5880d7732431eadd507e799cdd8.js"}],"tag":"script","inner":""}],{"target":"head"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-// Multitag
+/* Multitag */
 (function(){injectScript([{"attr":[{"name":"src","value":"https://fpyf8.com/88/tag.min.js"},{"name":"data-zone","value":"185210"},{"name":"data-cfasync","value":"false"}],"tag":"script","inner":""}],{"target":"head"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-// Floating Ads
+/* Floating */
 (function(){injectScript([{"attr":[{"name":"id","value":"fixedban"},{"name":"style","value":"width:100%;margin:auto;text-align:center;float:none;overflow:hidden;display:scroll;position:fixed;bottom:0;z-index:999;-webkit-transform:translateZ(0);"}],"tag":"div","inner":"<div style=\"margin-bottom: 2px;\"><center><a id=\"close-fixedban\" onclick=\"document.getElementById(&quot;fixedban&quot;).style.display = &quot;none&quot;;\" style=\"cursor:pointer;\"><img alt=\"close\" src=\"https://3.bp.blogspot.com/-ZZSacDHLWlM/VhvlKTMjbLI/AAAAAAAAF2M/UDzU4rrvcaI/s1600/btn_close.gif\" style=\"vertical-align:middle;\" title=\"close\"></a></center></div><div style=\"text-align:center;display:block;max-width:728px;height:auto;overflow:hidden;margin:auto\">\n<center id=\"ad__float\"></center></div>"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"atOptions = {'key' : '9d1510d86a088317e407113e9f5321f8','format' : 'iframe','height' : 250,'width' : 300,'params' : {}};"},{"attr":[{"name":"type","value":"text/javascript"},{"name":"src","value":"//hustlereadcoffeehouse.com/9d1510d86a088317e407113e9f5321f8/invoke.js"}],"tag":"script","inner":""}],{"target":"#ad__float"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
 
-// Histats
+/* Histats */
 (function(){injectScript([{"attr":[{"name":"type","value":"text/javascript"}],"tag":"script","inner":"var _Hasync= _Hasync|| [];_Hasync.push(['Histats.start', '1,4946035,4,0,0,0,00010000']);_Hasync.push(['Histats.fasi', '1']);_Hasync.push(['Histats.track_hits', '']);(function() {var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;hs.src = ('//s10.histats.com/js15_as.js');(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);})();"}],{"target":"body"});function injectScript(e,t){let n=t.target;for(let t of e){let e=t.tag,r=t.inner,o=document.createElement(e);o.innerHTML=r;let c=t.attr;for(let e of c)o.setAttribute(e.name,e.value);document.querySelector(n)&&document.querySelector(n).append(o)}}})();
